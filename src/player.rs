fb301fbe35256d4f396c77723733a10f1b6346ce
Mr Speaker <mrspeaker@gmail.com>
2024-02-24 13:52:56 +0800

    Raycast collisions

--- src/player.rs
+++ src/player.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 use bevy::input::mouse::MouseMotion;
 use crate::physics::{Impulse, Torque};
 use crate::laxer::Laxer;
+use bevy_mod_raycast::prelude::*;
 
 pub struct PlayerPlugin;
 
@@ -10,7 +11,8 @@ impl Plugin for PlayerPlugin {
         app.add_systems(Startup, init_player);
         app.add_systems(Update, (
             update_player,
-            auto_level
+            auto_level,
+            raycast
         ));
     }
 }
@@ -80,5 +82,30 @@ fn auto_level(
 ) {
     if let Ok(t) = q.get_single_mut() {
         gizmos.arrow(Vec3::ZERO, t.rotation.to_axis_angle().0, Color::YELLOW);
+
+    }
+}
+
+fn raycast(
+    mut raycast: Raycast,
+    mut gizmos: Gizmos,
+    mut q: Query<(&Transform, &mut Impulse), With<Player>>
+) {
+    if let Ok((t, mut i)) = q.get_single_mut() {
+        let ray = Ray3d::new(
+            t.translation,
+            t.rotation * -Vec3::Z
+        );
+        let hits = raycast.debug_cast_ray(ray, &RaycastSettings::default(), &mut gizmos);
+        if let Some((_, hit)) = hits.first() {
+            let dist = hit.distance();
+            if dist < 20.0 {
+                // Bounce back
+                i.add_force(
+                    t.back().normalize() * 0.1 +
+                        hit.normal().normalize() * 0.1
+                );
+            }
+        }
     }
 }
