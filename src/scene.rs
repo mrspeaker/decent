c3cb41424db4a0291494b45fb3f23d65407e8527
Mr Speaker <mrspeaker@gmail.com>
2024-03-05 12:38:33 +0800

    runif

--- src/scene.rs
+++ src/scene.rs
@@ -1,12 +1,14 @@
 use bevy::prelude::*;
 use std::f32::consts::PI;
+use crate::state::GameState;
 
 pub struct ScenePlugin;
 
 impl Plugin for ScenePlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(Startup, init_scene);
+            .add_systems(Startup, init_scene
+                         .run_if(in_state(GameState::Menu)));
     }
 }
 
